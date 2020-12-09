7dd75172ef023b96d77fac766e1f45bca2d850d9
Gabriel Felipe de Agostinho <g.felipeagostinho@gmail.com>
2020-12-09 11:33:14 -0300

    V1

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import os
+
+def extract(file):
+    path = os.getcwd() + r"\frames\\"
+    os.mkdir(path)
+    capture = cv2.VideoCapture(file)
+    i = 1
+
+    while(capture.isOpened()):
+        ret, frame = capture.read()
+        if ret == False:
+            print("Extraction complete!")
+            break
+        print("Extracting frame" + str(i))
+        cv2.imwrite(path + "Frame" + str(i) + ".png", frame)
+        i += 1
+
+    capture.release()
+    cv2.destroyAllWindows()
+    
+def main():
+    file = input(r"Please enter the path of the video (Example: C:\Users\user\Videos\video.mp4): ")
+    extract(file)
+
+main()
